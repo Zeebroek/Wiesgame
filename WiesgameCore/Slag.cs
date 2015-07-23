f3b54ce1d1dfb624b3bd059a5b6173091d4ed910
Senne <scheithoop@cisencarine.be>
2015-07-23 15:52:16 +0200

    lelk

---
+++ WiesgameCore/Slag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiesgameCore
+{
+    public class Slag
+    {
+        public Spel Spel { get; private set; }
+        public List<Kaart> Kaarten { get; set; }
+
+
+        public Slag(Spel spel)
+        {
+            this.Spel = spel;
+        }
+
+    }
+}
---
+++ WiesgameCore/Speler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WiesgameCore
+{
+    public class Speler
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        
+
+        public Speler(int id, string name)
+        {
+            this.ID = id;
+            this.Name = name;
+        }
+
+
+    }
+}
